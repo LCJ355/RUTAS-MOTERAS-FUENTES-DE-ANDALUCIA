89ee63eec60062e4bc3cbc88d976f874a6688df2
Usuario <usuario@local>
2026-07-31 03:34:38 +0200

    feat: tipo de acceso informativo con contador por municipio, eliminar Ambos

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'rm-cache-v3';
+const CACHE = 'rm-cache-v4';
 const URLS = ['/', 'index.html', 'manifest.json', 'fuentes_data.js', 'photos_data.js', 'pueblos_andalucia.js', 'icon-192.png', 'icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => clients.claim())); });
